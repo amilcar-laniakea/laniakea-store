11ea5246c98d85d508aa957bc8205525aef127fb
Amilcar Barahona <amilcar.laniakea@gmail.com>
2021-07-05 20:51:32 -0400

    implementada libreria sass

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ import Router from './router'
 
 import 'antd/dist/antd.compact.min.css'
 
+import './scss/style.scss'
+
 ReactDOM.render(<Router />, document.getElementById('root'))
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,8 @@
 /** @format */
 
-const Home = () => <h3>Inicio</h3>
+const Home = () => (
+	<div className='global-container'>
+		<h3>Inicio</h3>
+	</div>
+)
 export default Home
