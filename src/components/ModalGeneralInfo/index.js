f4006bfabde1ea7a35f2b7e4eefa845da15001ba
Fabiorlan Dyan <yhoyannaasakura1992@gmail.com>
2021-08-24 11:32:36 -0400

    Estilo de categorias listo 24-08-2021

--- src/components/ModalGeneralInfo/index.js
+++ src/components/ModalGeneralInfo/index.js
@@ -6,18 +6,21 @@ import { Modal } from 'antd'
 
 import { ContextGlobalConsumer } from '../../context/Global'
 
+import './style.scss'
+
 const ModalGeneralInfo = ({ info }) => {
 	const { isModalGeneralInfo, HandleModalGeneralInfo } = ContextGlobalConsumer()
 
 	return (
 		<Modal
+			wrapClassName='general-info-modal'
 			title={info.title}
 			visible={isModalGeneralInfo}
 			onOk={HandleModalGeneralInfo}
-			onCancel={HandleModalGeneralInfo}>
-			<p>{info.subtitle}</p>
-			<p>{info.description}</p>
-			<p>{info.sub_description}</p>
+			cancelButtonProps={{ style: { display: 'none' } }}>
+			<h3 className='general-info-modal-title'>{info.subtitle}</h3>
+			<h3 className='general-info-modal-title'>{info.description}</h3>
+			<h3 className='general-info-modal-title'>{info.sub_description}</h3>
 		</Modal>
 	)
 }
